67c3212df011b79292af7c77d78c3f0b22630a84
Fede <you@example.com>
2023-03-07 05:09:12 -0300

    cors

--- app.js
+++ app.js
@@ -16,9 +16,14 @@ const swaggerSpec = require('./src/helpers/swagger')
 const swaggerJsDoc = require('swagger-jsdoc');
 
 // Middlewares
-app.use(cors()) 
 app.use(express.json({limit: '25mb'}))
 app.use(express.urlencoded({ extended: false, limit: '25mb' }))
+app.use(cors()) 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Routes
 app.use('/admin', adminRoutes)
